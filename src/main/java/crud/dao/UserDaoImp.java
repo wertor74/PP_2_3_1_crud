b309949e966d070715003873e794ae7afda32e55
wertor <wertor74@gmail.com>
2022-07-04 21:33:22 +0300

    PP_2_3_1 start

--- src/main/java/crud/dao/UserDaoImp.java
+++ src/main/java/crud/dao/UserDaoImp.java
@@ -32,22 +32,7 @@ public class UserDaoImp implements UserDao {
 
     @Override
     public void updateUser(User user, Long id) {
-        entityManager.createQuery("UPDATE User u set u.name = :newName WHERE u.id =:oldId")
-                .setParameter("oldId", id)
-                .setParameter("newName", user.getName())
-                .executeUpdate();
-        entityManager.createQuery("UPDATE User u set u.fatherName =:newFatherName WHERE u.id =:oldId")
-                .setParameter("oldId", user.getId())
-                .setParameter("newFatherName", user.getFatherName())
-                .executeUpdate();
-        entityManager.createQuery("UPDATE User u set u.lastName =:newLastName WHERE u.id =:oldId")
-                .setParameter("oldId", user.getId())
-                .setParameter("newLastName", user.getLastName())
-                .executeUpdate();
-        entityManager.createQuery("UPDATE User u set u.age =:newAge WHERE u.id =:oldId")
-                .setParameter("oldId", user.getId())
-                .setParameter("newAge", user.getAge())
-                .executeUpdate();
+        entityManager.merge(user);
     }
 
     @Override
